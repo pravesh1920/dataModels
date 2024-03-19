2e659b14f6bc0c009c66d4bd8125a8ed24b1496b
pravesh1920 <praveshdevda19@gmail.com>
2024-03-19 08:04:50 +0530

    Todos data models

---
+++ models/todos/todo.models.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const todoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    subTodo: {
+      type: mongoose.Schema.type.ObjectId,
+      ref: 'SubTodo',
+    },
+  },
+  { timestamps: true }
+);
+
+export const Todo = mongoose.model('Todo', todoSchema);
