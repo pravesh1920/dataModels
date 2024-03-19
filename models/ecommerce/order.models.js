ea778b311e6908884e0ddcd72b0e058069c3e404
pravesh1920 <praveshdevda19@gmail.com>
2024-03-19 10:50:47 +0530

    Ecommerce data model

---
+++ models/ecommerce/order.models.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+const orderItemSchema = new mongoose.Schema(
+    {
+        productId: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    },
+    {
+        quantity: {
+            type: Number,
+            required: true
+        }
+    }
+);
+const orderShema = new mongoose.Schema(
+    {
+        orderPrice: {
+            type: Number,
+            required: true,
+        },
+        customer: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        orderItems: {
+            type: [orderItemSchema]
+        },
+        address: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            enum: ["PENDING", "CANCELLED", "DELIVERED"],
+            default: "PENDING"
+        }
+    }, {timestamps: true});
+
+export const Order = mongoose.model('Order', orderShema);
